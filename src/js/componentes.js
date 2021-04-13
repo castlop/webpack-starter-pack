75b7954f6c56b7a1290d3bac0de4e8a4dbbbda32
Michael Castillo <64052018+castlop@users.noreply.github.com>
2021-04-12 23:52:59 -0500

    Remove unnecessary code from components.js

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,14 +1,3 @@
 import '../css/componentes.css';
 
-export {saludar};
-
-const saludar = ( nombre ) => {
-  
-  console.log('Creando elemento h1')
-
-  const h1 = document.createElement('h1');
-  h1.innerText = `Hola ${nombre} con el Webpack DevServer!`
-
-  document.body.append(h1)
-
-}
+// export {};
